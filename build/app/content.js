257200b158ec95ff05622e05bcd3daf8645fba19
Owen Delisle <owen37164@gmail.com>
2019-03-11 18:12:31 -0700

    Realized searching throught the whole DOM was creating some bugs. Switching to searching by HTML formatting tags

--- build/app/content.js
+++ build/app/content.js
@@ -1,5 +1,5 @@
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  let paragraphs = document.getElementsByTagName("*");
+  let paragraphs = document.getElementsByTagName("p", "h1");
   message.msg.word.forEach(w => {
     for (elt of paragraphs) {
       temp = elt.innerHTML.replace(w.word, w.wordToChange);
